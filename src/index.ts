14e64ee71a7757b0e364986475037df3d17cd6ae
Hans Stemshaug <hans.stemshaug@ntnu.no>
2023-03-22 15:26:18 +0100

    eslint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin,
-  IRouter
-} from '@jupyterlab/application';
+import { JupyterFrontEnd, JupyterFrontEndPlugin, IRouter } from '@jupyterlab/application';
 
 import { Widget } from '@lumino/widgets';
 
@@ -51,8 +47,7 @@ const extension: JupyterFrontEndPlugin<void> = {
     const logout = document.createElement('button');
     logout.id = 'logout';
     logout.type = 'button';
-    logout.innerHTML =
-      '<span class="bp3-button-text"><i aria-hidden="true" class="fa fa-sign-out"></i> Logout</span>';
+    logout.innerHTML = '<span class="bp3-button-text"><i aria-hidden="true" class="fa fa-sign-out"></i> Logout</span>';
     logout.addEventListener('click', () => {
       router.navigate('/logout', { hard: true });
     });
